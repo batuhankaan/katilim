e4aa3cb954dd31ec8d35da71edfd97ca1eb0767c
batuhankaan <bkaanaribali@gmail.com>
2020-08-13 17:32:52 +0300

    up

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './src/pages/login/login';
+import authPhone from './src/pages/login/authPhone'
+import merchantSelect from './src/pages/login/merchantSelect'
+import deviceVerification from './src/pages/login/deviceVerification'
+import passwordReset from './src/pages/login/passwordReset'
+import Register from './src/pages/register/register'
+import newCustomerForm from './src/pages/register/newCustomerForm'
+
+const Drawer = createDrawerNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Otest</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Login">
+        <Drawer.Screen name="Login" component={Login} />
+        <Drawer.Screen name="authPhone" component={authPhone} />
+        <Drawer.Screen name="merchantSelect" component={merchantSelect} />
+        <Drawer.Screen name="deviceVerification" component={deviceVerification} />
+        <Drawer.Screen name="passwordReset" component={passwordReset} />
+        <Drawer.Screen name="Register" component={Register} />
+        <Drawer.Screen name="newCustomerForm" component={newCustomerForm} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
